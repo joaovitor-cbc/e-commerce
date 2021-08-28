6935303ff13bd530e29503b0db15f2164db27427
JoaoVitor_linux <joaovitor.novacruz@gmail.com>
2021-08-28 10:24:09 -0300

    Adicioando repósitory da classe produto

---
+++ src/main/java/com/ecommerce/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+    Optional<Produto> findByNome(String nome);
+}
