ede4552448ff2b1af8e5d75f4b5929473e27cc84
JoaoVitor_linux <joaovitor.novacruz@gmail.com>
2021-08-28 10:14:13 -0300

    Adicionando controller para a classe venda

---
+++ src/main/java/com/ecommerce/controller/VendaController.java
@@ -0,0 +1,23 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.model.Venda;
+import com.ecommerce.service.VendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/iniciar-compra")
+public class VendaController {
+
+    @Autowired
+    private VendaService vendaService;
+
+
+    @PostMapping("/registrar")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Venda iniciarCompra(@RequestBody Venda venda){
+        return vendaService.criarVenda(venda);
+    }
+
+}
