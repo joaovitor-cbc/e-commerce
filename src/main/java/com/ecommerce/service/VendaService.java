15db91e2a8b1451689f75b3229618e3c0cb66838
JoaoVitor_linux <joaovitor.novacruz@gmail.com>
2021-08-28 10:49:49 -0300

    Adicionando metodo no service de venda para validar usuario

--- src/main/java/com/ecommerce/service/VendaService.java
+++ src/main/java/com/ecommerce/service/VendaService.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 @Service
 public class VendaService {
 
@@ -17,9 +19,12 @@ public class VendaService {
     private UsuarioService usuarioService;
 
     public Venda criarVenda(Venda venda){
-        if(!usuarioService.verificarExistenciaUsuario(venda.getUsuario().getCpf())){
+        validarVendaUsuario(venda.getUsuario().getCpf());
+        return vendaRepository.save(venda);
+    }
+    public void validarVendaUsuario(String cpfUsuario){
+        if(!usuarioService.verificarExistenciaUsuario(cpfUsuario)){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "usuario não cadastrado");
         }
-        return vendaRepository.save(venda);
     }
 }
