7996c1abdab58db058d9885d38578f35da21cc2d
JoaoVitor_linux <joaovitor.novacruz@gmail.com>
2021-09-11 19:58:29 -0300

    Adicionando metodos para mapeamento de DTO no service do Usuario

--- src/main/java/com/ecommerce/domain/service/UsuarioService.java
+++ src/main/java/com/ecommerce/domain/service/UsuarioService.java
@@ -1,7 +1,10 @@
 package com.ecommerce.domain.service;
 
+import com.ecommerce.DTO.UsuarioInput;
+import com.ecommerce.DTO.UsuarioModel;
 import com.ecommerce.domain.model.Usuario;
 import com.ecommerce.domain.repository.UsuarioRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,9 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Usuario savarUsuario(Usuario usuario){
         if (verificarExistenciaUsuario(usuario.getCpf())){
@@ -33,4 +39,13 @@ public class UsuarioService {
         Usuario usuario = usuarioRepository.findById(idUsuario)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "usuario não cadastrado"));
     }
+
+    private UsuarioModel toUsuarioModel(Usuario usuario){
+        return modelMapper.map(usuario, UsuarioModel.class);
+    }
+
+    private Usuario toUsuarioEntity(UsuarioInput usuarioInput){
+        return modelMapper.map(usuarioInput, Usuario.class);
+    }
+
 }
